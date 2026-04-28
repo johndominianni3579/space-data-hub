cde5dab59d129f8ed6748358ea80931bb876fbd1
John Dominianni <jmd6747@rit.edu>
2026-04-28 16:52:54 -0400

    updated artemis 2 mission status

--- collectors/nasa_artemis_api.py
+++ collectors/nasa_artemis_api.py
@@ -3,7 +3,7 @@
 def get_artemis_updates():
     return [
         {"name": "Artemis I", "status": "COMPLETED (December 2022)", "goal": "An uncrewed flight test of SLS and Orion, Artemis I Spent 25 days in space, completing a Distant Retrograde Orbit around the moon before splashing down into the Pacific Ocean.", "image": "artemis1.jpeg"},
-        {"name": "Artemis II", "status": "IN PROGRESS (Launched April 1, 2026)", "goal": "Artemis II is the first crewed mission of the Artemis program! Four astronauts are currently performing a 10-day lunar flyby to test life support systems onboard the Integrity Orion Capsule.", "image": "artemis2.jpeg"},
+        {"name": "Artemis II", "status": "COMPLETED (April 2026)", "goal": "Artemis II is the first crewed mission of the Artemis program! Four astronauts are currently performing a 10-day lunar flyby to test life support systems onboard the Integrity Orion Capsule.", "image": "artemis2.jpeg"},
         {"name": "Artemis III", "status": "Scheduled for 2027", "goal": "Now a Low Earth Orbit (LEO) rehearsal, Artemis III Will test docking and rendezvous between Orion and the SpaceX Starship HLS. The HLS (Human Landing System) will be used to land on the moon", "image": "artemis3.jpeg"},
         {"name": "Artemis IV", "status": "Targeting early 2028", "goal": "It will be official human return to the lunar surface! It will be the first mission to land humans on the Moon since Apollo 17 in 1972.", "image": "artemis4.jpeg"},
         {"name": "Artemis V", "status": "Targeting late 2028", "goal": "As the second human landing on the Moon, Artemis V will mark the beginning of deploying infrastructure on moon for a permanent human base on the lunar south pole", "image": "artemis5.jpeg"}
